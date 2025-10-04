45fd36d52fb3465088cdc965c34f46d4db3b280e
Jose Carreiro <jmcgisc@gmail.com>
2025-10-04 15:46:00 -0600

    fix:email

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { HelmetProvider } from "react-helmet-async"
+import { HelmetProvider } from "../node_modules/react-helmet-async/lib/index.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
