b3f4b0dade1ee88e9cd4b84bc0d00f342f8af10e
Jose Carreiro <jmcgisc@gmail.com>
2025-10-15 19:19:02 -0600

    feature:hasta que paguen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        {/* <App /> */}
+        <App />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
